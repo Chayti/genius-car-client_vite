79e88ff4b5a457f24f6b015e2d9a441e804d0134
Chayti Saha <chaytisaha98@gmail.com>
2022-10-25 15:39:11 +0600

    all components setup done for home page

---
+++ src/Pages/Home/ServiceArea/ServiceArea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServiceArea = () => {
+    return (
+        <div>
+            <h1>This is servicearea</h1>
+        </div>
+    );
+};
+
+export default ServiceArea;
---
+++ src/Pages/Home/TopCarousel/TopCarousel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopCarousel = () => {
+    return (
+        <div>
+            <h1>This is TopCarousel</h1>
+        </div>
+    );
+};
+
+export default TopCarousel;
