89c717615db4aed66312294fdb567816146457bb
Chayti Saha <chaytisaha98@gmail.com>
2022-10-26 02:42:38 +0600

    rating added in products section

--- src/Pages/Home/Products/SingleProduct/SingleProduct.js
+++ src/Pages/Home/Products/SingleProduct/SingleProduct.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Rating from 'react-rating';
+import { FaStar } from 'react-icons/fa';
 
 const SingleProduct = ({ product }) => {
     const { title, price, img, rating } = product
     return (
         <div className="card glass rounded-lg">
             <div className="card-body mx-4 text-center">
-                <figure><img className="rounded-lg h-32 bg-gray-200 md:px-20 md:py-2" src={img} alt="car!" /></figure>
+                <figure><img className="rounded-lg h-32 bg-gray-300 px-20 py-2" src={img} alt="car!" /></figure>
+                <Rating
+                    readonly
+                    initialRating={rating}
+                    emptySymbol={<FaStar className="text-gray-300" />}
+                    fullSymbol={<FaStar className="text-orange-600" />}
+                />
                 <h2 className="text-xl mt-2">{title}</h2>
                 <p className="text-orange-700 font-bold">${price}</p>
             </div>
