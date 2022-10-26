662d59f9742da22b6ce29ed5f1658a6c126f63f5
ShahbajKhan <shktoky@gmail.com>
2022-10-26 16:56:38 +0600

    worked on authentication

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import illustration from "../../assets/others/Illustration.svg";
 import SocialAuth from "../../components/SocialAuthentication/SocialAuth";
+import { AuthContext } from "../../contexts/AuthProvider";
 const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [error, setError] = useState("");
+  const { signIn, setLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        setError("");
+        if (user?.uid) {
+          navigate(from, { replace: true });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
   return (
     <section className="container mx-auto p-12 h-full">
       <div className="grid md:grid-cols-2">
         <div className="">
-          <img src={illustration} className="h-4/5 block mx-auto" alt="Password_image" />
+          <img
+            src={illustration}
+            className="h-4/5 block mx-auto"
+            alt="Password_image"
+          />
         </div>
         <div className="">
           <h1 className="font-bold text-center text-3xl">Login</h1>
           <form onSubmit={handleSubmit} className="md:w-1/2 mx-auto">
-            
             <div className="form-control w-full max-w-xs mb-6">
               <label className="label">
                 <span className="label-text font-bold">Email</span>
               </label>
               <input
                 type="text"
+                name="email"
                 placeholder="Type here"
                 className="input input-bordered w-full max-w-xs"
               />
@@ -30,13 +63,13 @@ const Login = () => {
                 <span className="label-text font-bold">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
+                name="password"
                 placeholder="Type here"
                 className="input input-bordered w-full max-w-xs"
               />
             </div>
             <div className="form-control w-full max-w-xs mb-6">
-              
               <input
                 type="submit"
                 value={"Sign In"}
@@ -44,7 +77,11 @@ const Login = () => {
               />
             </div>
           </form>
-          <SocialAuth/>
+
+          <div className="md:w-1/2 mx-auto">
+            <p className="font-semibold text-center my-6">Or sign in with</p>
+            <SocialAuth navigate={navigate} from={from} />
+          </div>
         </div>
       </div>
     </section>
