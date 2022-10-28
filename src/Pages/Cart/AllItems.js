b0a8ce1376c0ffd46bc76495d514bf660f46a469
Chayti Saha <chaytisaha98@gmail.com>
2022-10-28 09:33:46 +0600

    updated

--- src/Pages/Cart/AllItems.js
+++ src/Pages/Cart/AllItems.js
@@ -1,15 +1,11 @@
 import React from "react";
 import SingleItem from "./SingleItem";
 
-const AllItems = ({orders}) => {
+const AllItems = ({ orders }) => {
   return (
     // Used a table from daisyUI. 
     // Table starting
-<<<<<<< HEAD
-    <div className="overflow-x-auto my-10 w-full">
-=======
     <div className=" my-10 overflow-x-auto w-full">
->>>>>>> e1b6fd4c02d18556c6f6eafc4a1ae8c2e25aa7f1
       <table className="table w-full">
         <thead>
           <tr>
@@ -23,7 +19,7 @@ const AllItems = ({orders}) => {
         <tbody>
           {/* Show single item of cart by using map */}
           {
-            orders?.map(order=> <SingleItem order={order}/>)
+            orders?.map(order => <SingleItem order={order} />)
           }
         </tbody>
       </table>
--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -4,10 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider";
 import Banner from "../SingleService/Banner/Banner";
 import AllItems from "./AllItems";
-<<<<<<< HEAD
-=======
-import CartBanner from "./CartBanner";
->>>>>>> e1b6fd4c02d18556c6f6eafc4a1ae8c2e25aa7f1
 
 const Cart = () => {
   const { user } = useContext(AuthContext);
@@ -20,11 +16,7 @@ const Cart = () => {
       .then((data) => setOrders(data));
   }, [user]);
   return (
-<<<<<<< HEAD
-    <section className="w-5/6 mx-auto px-6">
-=======
     <section className="container mx-auto px-6 md:w-5/6">
->>>>>>> e1b6fd4c02d18556c6f6eafc4a1ae8c2e25aa7f1
       {/* Banner using Hero */}
       <Banner title={"My Orders"} />
       {/* This component shows all the orders of a specific email. */}
@@ -32,14 +24,14 @@ const Cart = () => {
 
       {/* This contains  Clear Shopping Cart and Continue Shopping button*/}
       <div className="my-4 grid md:grid-cols-2">
-        
-          <button onClick={()=>{navigate(`/services`)}} className="mb-2 flex items-center justify-center">
-            <span className="mr-2">
-              <FaReply />
-            </span>{" "}
-            Continue Shopping
-          </button>
-        
+
+        <button onClick={() => { navigate(`/services`) }} className="mb-2 flex items-center justify-center">
+          <span className="mr-2">
+            <FaReply />
+          </span>{" "}
+          Continue Shopping
+        </button>
+
         <button className="flex items-center justify-center">
           <span className="mr-2">
             {/* Icon form react-icons */}
