baad6e580a8c1c8e7934c19b0c6ad354e9eecdb2
Chayti Saha <chaytisaha98@gmail.com>
2022-10-30 15:13:09 +0600

    sorting by price

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -3,22 +3,32 @@ import Banner from "../SingleService/Banner/Banner";
 import ServicesTable from "./ServicesTable";
 
 const ManageServices = () => {
-    const [services, setServices] = useState([]);
-    const [refetch, setRefetch] = useState(false)
-    useEffect(() => {
-        fetch(`http://localhost:5000/services`)
-          .then((res) => res.json())
-          .then((data) => setServices(data));
-      }, [refetch]);
+  const [services, setServices] = useState([]);
+  const [refetch, setRefetch] = useState(false)
+  const [sorted, setSorted] = useState(false)
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/services`)
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, [refetch]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/sortedServices`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (sorted) setServices(data)
+      });
+  }, [sorted]);
   return (
     <section className="container mx-auto px-6 md:w-5/6">
       {/* Banner using Hero */}
       <Banner title={"Manage Services"} />
       {/* This component shows all the orders of a specific email. */}
-      <ServicesTable services={services} setRefetch={setRefetch}/>
+      <ServicesTable services={services} setRefetch={setRefetch} setSorted={setSorted} />
 
       {/* This contains  Clear Shopping Cart and Continue Shopping button*/}
-      
+
     </section>
   );
 };
